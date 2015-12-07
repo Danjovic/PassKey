d390b68f3f2f79ea9fca1f857b824e45f35d42fa
Danjovic <danjovic@hotmail.com>
2015-12-07 20:17:26 -0200

    Support for <CR> in legacy passwords file

--- utils/pass2bin.py
+++ utils/pass2bin.py
@@ -5,6 +5,9 @@
 # Version 0.1 - December 2, 2015
 # Basic Release
 #
+# Version 0.11 - December 7, 2015
+# Added support for <ENTER> at the end of password
+#
 # This code is released under GPL V2.0
 # 
 # This script reads a text file with your legacy passwords
@@ -12,21 +15,8 @@
 # a command line with arguments for avrdude to upload them
 # to KeyPass.
 #
-# Rules for the text file:                         
-#  - Filename must be passwords.txt                
-#  - One password per line                         
-#  - Maximum length 12 characters                  
-#  - Maximum 10 passwords                          
-#  - Blank lines will be ignored                   
-#  - Lines before a starting mark will be ignored  
-#  - Blank lines will be ignored                   
-#  - The starting mark is a tripple percent signal 
-#    at the beginning of a line                    
-#  - Valid Character set: a..z A..Z 0..9 . _ - (space) (tab)                                 
-#  - Trailing spaces will be ignored if the line   
-#    length is more than 12 charactes. if your     
-#    password has trailing spaces leave the rest   
-#    of the line blank 
+# Read 'password_rules.txt' for more info.
+#
 
 ## FUNCTIONS ##
 def pad_with_zeroes(s):  # Keypass reserves 14 bytes for each password
@@ -44,6 +34,7 @@ try:
     print "Found password file. Parsing"
     passes = 0   # passwords found
     seeking = 1
+    cr_at_end = 0 
     while seeking:
         line = fi.readline()  # discard lines up to first %%% found  
         if not line: break
@@ -54,11 +45,16 @@ try:
         line = fi.readline()
         if not line: break
         line = line.strip('\n') #strip the end of line
-        linesize = len(line)        
+        linesize = len(line)
+        if linesize>12 and line[12]=="#":
+                cr_at_end = 1
+        else: cr_at_end =0
+        
         line = line[0:12]       # limit the number of characters
         if linesize>12:
             line=line.rstrip()  # trim spaces for lines with comments
         if len(line)!=0:
+            if cr_at_end: line = line + chr(10)
             passwords[passes]=line
             passes=passes+1              
     fi.close()                  # Reached End of file of found all passwords
